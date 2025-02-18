8239b5b3bce6a58de179fde220bda8001fd41425
youth <boysoeng@naver.com>
2025-02-18 23:37:45 +0900

    feat: 기본 페이지 설정

---
+++ src/main/java/com/youth/manito/controller/ViewController.java
@@ -0,0 +1,17 @@
+package com.youth.manito.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+@RequiredArgsConstructor
+public class ViewController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
