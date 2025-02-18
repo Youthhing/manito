515ce3350e9ebc8ddfc8617a0b37303d6b04177d
youth <boysoeng@naver.com>
2025-02-19 00:35:17 +0900

    feat: mission entity 추가

---
+++ src/main/java/com/youth/manito/domain/entity/Mission.java
@@ -0,0 +1,25 @@
+package com.youth.manito.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Mission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "contents", nullable = false, columnDefinition = "TEXT")
+    private String contents;
+
+    private boolean deleted;
+}
