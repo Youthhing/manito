2467fac4161f0d8a880f9bddc7da51ae2fb16529
youth <boysoeng@naver.com>
2025-02-18 23:44:23 +0900

    feat: team 및 user entity 설정

---
+++ src/main/java/com/youth/manito/domain/entity/Team.java
@@ -0,0 +1,28 @@
+package com.youth.manito.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User admin;
+}
---
+++ src/main/java/com/youth/manito/domain/entity/User.java
@@ -0,0 +1,29 @@
+package com.youth.manito.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "sessionKey")
+    private String sessionKey;
+}
