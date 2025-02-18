5df2a29cf5c536bc89e516289cc9907c1b16ed8a
youth <boysoeng@naver.com>
2025-02-18 23:53:17 +0900

    feat: team main page 설정

--- src/main/java/com/youth/manito/controller/ViewController.java
+++ src/main/java/com/youth/manito/controller/ViewController.java
@@ -1,17 +1,28 @@
 package com.youth.manito.controller;
 
+import com.youth.manito.service.TeamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
 @RequiredArgsConstructor
 public class ViewController {
 
+    private final TeamService teamService;
+
     @GetMapping
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/team")
+    public String team(@RequestParam("code") String code, Model model) {
+        model.addAttribute("team", teamService.getByCode(code));
+        return "team";
     }
 }
