63d71297a6c67e23586e0f455a55356f40e4d6d8
youth <boysoeng@naver.com>
2025-02-18 23:58:11 +0900

    feat: 불필요한 admin 개념 제거

--- src/main/java/com/youth/manito/domain/entity/Team.java
+++ src/main/java/com/youth/manito/domain/entity/Team.java
@@ -1,11 +1,9 @@
 package com.youth.manito.domain.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,7 +20,4 @@ public class Team {
     private String name;
 
     private String code;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User admin;
 }
