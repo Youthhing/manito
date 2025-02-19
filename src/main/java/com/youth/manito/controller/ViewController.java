0ba4aec5c3bf6a714ff07366a62dd124f5d85269
youth <boysoeng@naver.com>
2025-02-19 17:04:27 +0900

    feat: 마니또 투표 화면 구현

--- src/main/java/com/youth/manito/controller/ViewController.java
+++ src/main/java/com/youth/manito/controller/ViewController.java
@@ -1,6 +1,7 @@
 package com.youth.manito.controller;
 
 import com.youth.manito.service.TeamService;
+import com.youth.manito.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,8 @@ public class ViewController {
 
     private final TeamService teamService;
 
+    private final UserService userService;
+
     @GetMapping
     public String index() {
         return "index";
@@ -24,5 +27,11 @@ public class ViewController {
     public String team(@RequestParam("code") String code, Model model) {
         model.addAttribute("team", teamService.getByCode(code));
         return "team";
+    }
+
+    @GetMapping("/vote")
+    public String vote(@RequestParam("giverEmail") String email, @RequestParam("sessionKey") String sessionKey, Model model) {
+        model.addAttribute("user", userService.getByEmailAndKey(email, sessionKey));
+        return "vote";
     }
 }
